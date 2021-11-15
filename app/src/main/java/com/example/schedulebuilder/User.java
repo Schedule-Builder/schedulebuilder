0014e52a1cae3cbe9dab9212d9f0c115c31cf412
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2021-11-15 15:19:57 -0500

    Update User.java
    Added comments for the variables so they're easy to understand

--- app/src/main/java/com/example/schedulebuilder/User.java
+++ app/src/main/java/com/example/schedulebuilder/User.java
@@ -2,9 +2,9 @@ package com.example.schedulebuilder;
 
 public class User {
     
-    private boolean militaryTime = false;
-    private boolean cardDisplay = false;
-    private boolean timeTracker = false;
+    private boolean militaryTime = false; // this variable decides whether or not the times for the events will be in military time (true) or not (false)
+    private boolean cardDisplay = false; // this variable likely won't be used for a while- it will activate a specialized view that is a stretch goal
+    private boolean timeTracker = false; // this will decide if the user would want to notify when they finish activities in order to keep track of how they stick to their schedule
     
     
     public boolean getMilitaryTime(){
