d4fc1b8ba301e78555c1b0b4a369a2d2c74c6432
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2021-12-01 16:14:02 -0500

    Update ToDo.java
    In progress- putting in the auto-add function with the ToDo class

--- app/src/main/java/com/example/schedulebuilder/ToDo.java
+++ app/src/main/java/com/example/schedulebuilder/ToDo.java
@@ -1,12 +1,26 @@
 package com.example.schedulebuilder;
 
+
 public class ToDo {
   private String name;
   private int minTime;
   private int maxTime;
   private int dueDate;
   private int dueTime;
-  private int startTime;
-  private int endTime;
+  private ArrayList<Integer> startTime = new ArrayList<Integer>();
+  private ArrayList<Integer> endTime = new ArrayList<Integer>();
   private float priority;
+  
+  public void autoMakeEvent(Day day){
+    if (startTime.size() == 0){
+      day.byPriority.get(0)
+    }
+    else{
+      
+    }
+  }
+  
+  public void makeEvent(){
+    // takes to event creation screen, inserting toDo values for the event
+  }
 }
