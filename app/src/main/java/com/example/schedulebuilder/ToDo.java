474a308eb1ed840ac9d824a3a1d85b021b8e1bcf
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2021-12-01 14:25:08 -0500

    Update ToDo.java
    Creates the variables that ToDo will use.

--- app/src/main/java/com/example/schedulebuilder/ToDo.java
+++ app/src/main/java/com/example/schedulebuilder/ToDo.java
@@ -1,4 +1,12 @@
 package com.example.schedulebuilder;
 
 public class ToDo {
+  private String name;
+  private int minTime;
+  private int maxTime;
+  private int dueDate;
+  private int dueTime;
+  private int startTime;
+  private int endTime;
+  private float priority;
 }
