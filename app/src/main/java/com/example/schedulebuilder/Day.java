0ccf07abe689771374eb5afc1b43509cd76e9072
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2021-12-01 13:23:17 -0500

    Create Day.java
    This class creates two arrays of events, one sorted by time and the other sorted by priority, and automatically sorts them into both arrays with the push method. This creates the two views that come with each day of the week- one sorted by the time that it starts, and one sorted by the priority level. There is also a print method that can check and ensure that everything is sorted correctly.

---
+++ app/src/main/java/com/example/schedulebuilder/Day.java
@@ -0,0 +1,43 @@
+package com.example.schedulebuilder;
+
+import java.util.ArrayList;
+
+public class Day {
+  ArrayList<Event> byTime = new ArrayList<Event>();
+  ArrayList<Event> byPriority = new ArrayList<Event>();
+  
+  public ArrayList<Event> getByTime(){
+    return byTime;
+  }
+  
+  public ArrayList<Event> getByPriority(){
+    return byPriority;
+  }
+
+  public void push(Event e){
+    int counter = byTime.size();
+    for (int i = 0; i<counter; i++)
+      if (e.getStartTime() <= bytime.get(i).getStartTime()){
+        byTime.add(i,e);
+        break;
+      }
+    if (counter == byTime.size())
+      byTime.add(e);
+      
+    for (int i = 0; i<counter; i++)
+      if (e.getPriority() <= byPriority.get(i).getPriority()){
+        byPriority.add(i,e);
+        break;
+      }
+    if (counter == byPriority.size())
+      byPriority.add(e);
+  }
+
+  public void print(){
+    for (int i = 0; i<byTime.size(); i++)
+      System.out.println(byTime.get(i));
+    System.out.println();
+    for (int i = 0; i<byPriority.size(); i++)
+      System.out.println(byPriority.get(i));
+  }
+}
