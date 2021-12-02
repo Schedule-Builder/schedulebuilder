ea76b33cd989c4cf1b19388acf4340a25d0df169
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2021-12-01 22:02:58 -0500

    Update Day.java
    Added find functions to search by name and return the index for the two array lists included in the class.

--- app/src/main/java/com/example/schedulebuilder/Day.java
+++ app/src/main/java/com/example/schedulebuilder/Day.java
@@ -32,6 +32,20 @@ public class Day {
     if (counter == byPriority.size())
       byPriority.add(e);
   }
+  
+  public int findInTime(Event e){
+    for (int i = 0; i < byTime.size(); i++)
+      if (e.getName().equals(byTime.get(i).getName()))
+        return i;
+    return -1;
+  }
+    
+  public int findInPriority(Event e){
+    for (int i = 0; i < byPriority.size(); i++)
+      if (e.getName().equals(byPriority.get(i).getName()))
+        return i;
+    return -1;
+  }
 
   public void print(){
     for (int i = 0; i<byTime.size(); i++)
